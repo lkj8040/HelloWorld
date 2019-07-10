18d52543d8cf2ca018ed893a6bd721afb6bdd7ee
Administrator <Administrator@win-10>
2019-07-10 09:50:31 +0800

    modify

--- src/hello/helloworld.java
+++ src/hello/helloworld.java
@@ -3,6 +3,6 @@ package hello;
 public class helloworld {
  
 	public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World - modify");
     }
 }
